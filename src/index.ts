89234150a8d6ee4edcf8b9e55f8ad0520794b22f
LukeAnger <anger.luke@gmail.com>
2023-04-08 17:44:37 -0400

    basic voice recognition added

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,27 @@ const plugin: JupyterFrontEndPlugin<void> = {
   activate: (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension jupyter-voice-comments is activated!');
 
+    const SpeechRecognition =
+      (window as any).SpeechRecognition ||
+      (window as any).webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+
+    recognition.addEventListener('result', (event: any) => {
+      const last = event.results.length - 1;
+      const text = event.results[last][0].transcript;
+      console.log('Text: ' + text);
+    });
+
+    const startRecording = () => {
+      recognition.start();
+      console.log('Voice activated, try speaking into the microphone.');
+    };
     const widget = new Widget();
     widget.id = 'lm-VoiceWidget';
     const button = document.createElement('button');
     button.id = 'lm-VoiceWidget-button';
+    button.addEventListener('click', startRecording);
     widget.node.appendChild(button);
     console.log('APP SHELL', app.shell);
     app.shell.add(widget, 'top');
